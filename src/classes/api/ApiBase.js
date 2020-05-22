3dd92ad2d351a7cbeb1ca7a970d45e594a4cd9e9
Villena Guillaume <contact@guillaumevillena.fr>
2020-05-22 16:10:30 +0200

    Fixed base URL define

--- src/classes/api/ApiBase.js
+++ src/classes/api/ApiBase.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // You can use your own logic to set your local or production domain
 const params = new URLSearchParams(window.location.search)
-const baseDomain = params.has("url")? "http://"+params.get("url")+"/" : "http://example.com/";
+const baseDomain = params.has("url")? params.get("url") : "http://example.com/";
 // The base URL is empty this time due we are using the jsonplaceholder API
 
 const baseURL = `${baseDomain}`;
